ea1a83f0b5418f0181ee38587b8b8c17c671bfc8
Sam Michaels Rodriguez <smichae9@uwo.ca>
2023-12-14 17:49:03 -0500

    created the main screen layout

---
+++ src/main/java/com/example/pomodoro/MainScreenController.java
@@ -0,0 +1,19 @@
+package com.example.pomodoro;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class MainScreenController {
+    @FXML
+    private ImageView imageView;
+
+    public void startTimer() {
+    }
+
+    public void restartTimer() {
+    }
+
+    public void openSettings() {
+    }
+}
